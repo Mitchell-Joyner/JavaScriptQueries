36a780e796b3401ed18166a0539650a56885c488
Mitchell-Joyner <8mjoyner@stpatrick.org>
2017-03-22 14:04:01 -0500

    JavaScriptQueries - Fantastically kek-tastic. Ynot.
    Ynot. Ynot. Ynot. Ynot. Ynot. Ynot. Super Kek-tastic.

---
+++ click.js
@@ -0,0 +1,9 @@
+//declare samecolor() here
+function samecolor(){
+  let newcolor = #FFA500;
+  let div = querySelector('#samecolor div');
+  for(let c = 0;c<div.length;c++){
+
+  }
+}
+//declare diffcolor() here
