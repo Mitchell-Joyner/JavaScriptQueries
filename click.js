dfb2e088007d6ffdb9e1e5edb840f60c7f4f9914
Mitchell-Joyner <8mjoyner@stpatrick.org>
2017-03-23 13:56:51 -0500

    2nd Day of kek-tastic JavaQueries. Ynot.
    Ynot. Ynot. Ynot Ynot. Kek-tastic. Ynot.

--- click.js
+++ click.js
@@ -1,9 +1,18 @@
 //declare samecolor() here
 function samecolor(){
-  let newcolor = #FFA500;
-  let div = querySelector('#samecolor div');
+  let newcolor = "#FFA500";
+  let div = querySelectorAll("#samecolor div");
+  for(let c = 0;c<div.length;c++){
+  div[c]style.backgroundColor = "#FFA500";
+  div[c]style.opacity += .25*[c];
+  }
+}
+//declare diffcolor() here
+function diffcolor(){
+  let color = "#FFA500";
+  let complement = "#007FFF";
+  let div = querySelectorAll("#diffcolor div");
   for(let c = 0;c<div.length;c++){
 
   }
 }
-//declare diffcolor() here
