f56ad4c04c86259087faca3744cd4f11f7d9409d
Mitchell-Joyner <8mjoyner@stpatrick.org>
2017-03-24 13:59:39 -0500

    Kek-tastic. Ynot.
    Ynot. Ynot. Ynot.

--- click.js
+++ click.js
@@ -1,18 +1,23 @@
 //declare samecolor() here
 function samecolor(){
   let newcolor = "#FFA500";
-  let div = querySelectorAll("#samecolor div");
+  let div = document.querySelectorAll("#samecolor div");
   for(let c = 0;c<div.length;c++){
-  div[c]style.backgroundColor = "#FFA500";
-  div[c]style.opacity += .25*[c];
+  div[c].style.backgroundColor = newcolor;
+  div[c].style.opacity = 0.25*c;
   }
 }
 //declare diffcolor() here
 function diffcolor(){
-  let color = "#FFA500";
-  let complement = "#007FFF";
-  let div = querySelectorAll("#diffcolor div");
+  let colors = "#FFA500";
+  let complement = "‎#007FFF";
+  let div = document.querySelectorAll("#diffcolor div");
   for(let c = 0;c<div.length;c++){
-
+    if(c%2 == 0){
+      div[c].style.backgroundColor = "#FFA500";
+    }
+    else{
+      div[c].style.backgroundColor = "#007FFF";
+    }
   }
 }
